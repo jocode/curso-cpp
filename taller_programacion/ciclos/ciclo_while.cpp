4b611f2dec99a2ced8cf0667ec11c139810bac65
Johan Mosquera <joncamilomoni@gmail.com>
2019-05-31 12:43:35 -0500

    Ejercicios usando ciclo While
    Se suben los ejercicios usando ciclo while. Imprimir números del 1 al 10, imprimir la palabra Usco 10 veces y imprimir los números pares e impares dado un intervalo.

---
+++ taller_programacion/ciclos/ciclo_while.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int i = 1;
+	
+	while (i <= 10){
+		cout << i << endl;
+		i++;
+	}
+	
+	getch();
+	
+	return 0;
+}
+
---
+++ taller_programacion/ciclos/ciclo_while_printf.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	int i = 1;
+	
+	while (i <= 10){
+		printf("%d \n", i);
+		i++;
+	}
+	
+	getch();
+	
+	return 0;
+}
+
